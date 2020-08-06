c411db2aba52c368015725397a964e3700bcf417
Andres Solano <andres.solano01@outlook.com>
2020-08-05 22:04:55 -0600

    Logger

--- 01-Maven/hello-maven/src/main/java/com/andres/App.java
+++ 01-Maven/hello-maven/src/main/java/com/andres/App.java
@@ -1,13 +1,19 @@
 package com.andres;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private final static Logger log = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from Maven!" );
+        log.info("Hello info");
+        log.debug("Hello debug");
+        log.warn("Hello warn");
     }
 }
